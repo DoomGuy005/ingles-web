29bfad6831ed7bd85dc823f57bab4deae1144146
Jherod Brendon <jherodjb@gmail.com>
2021-01-24 00:52:09 -0300

    site pronto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mostrarEN() {
+    document.getElementById("en").style.display = "flex";
+    document.getElementById("pt-br").style.display = "none";
+}
+
+function mostrarPT() {
+    document.getElementById("en").style.display = "none";
+    document.getElementById("pt-br").style.display = "flex";
+}
